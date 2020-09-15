e6c90f6152195bd39ddffa1b86b4ff16764b23e9
Pedro Bacchini <pedrobacchini@outlook.com>
2020-09-15 11:04:01 -0400

    Refatoração removendo excecões a mais que foram tratadas.

--- src/main/java/br/com/zup/bootcamp/proposta/advice/ExceptionHandlerAdvice.java
+++ src/main/java/br/com/zup/bootcamp/proposta/advice/ExceptionHandlerAdvice.java
@@ -4,15 +4,25 @@ import br.com.zup.bootcamp.proposta.config.LocaleMessageSource;
 import br.com.zup.bootcamp.proposta.dto.ErroApi;
 import br.com.zup.bootcamp.proposta.exception.ApiException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @RequiredArgsConstructor
-public class ResourceExceptionHandlerAdvice {
+public class ExceptionHandlerAdvice {
 
     private final LocaleMessageSource localeMessageSource;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroApi> handleMethodArgumentNotValid(MethodArgumentNotValidException methodArgumentNotValidException) {
+        String mensagem = localeMessageSource.obterMensagem("erro-validacao");
+        ErroApi erroApi = new ErroApi(HttpStatus.BAD_REQUEST, mensagem);
+        erroApi.adicionarBindingResult(methodArgumentNotValidException.getBindingResult(), localeMessageSource);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erroApi);
+    }
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErroApi> manipuladorApiException(ApiException ex) {
