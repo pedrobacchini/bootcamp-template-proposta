54f3f09b1d6ab0ac05b12e066228b54bc27fc05f
Pedro Bacchini <pedrobacchini@outlook.com>
2020-09-18 08:22:13 -0400

    Configuração dos tempos de resposta e variaveis de configuração

--- src/main/java/br/com/zup/bootcamp/proposta/repository/PropostaRepository.java
+++ src/main/java/br/com/zup/bootcamp/proposta/repository/PropostaRepository.java
@@ -3,11 +3,13 @@ package br.com.zup.bootcamp.proposta.repository;
 import br.com.zup.bootcamp.proposta.entity.Proposta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
 
+    @Transactional(readOnly = true)
     Optional<Proposta> findByDocumento(String documento);
 }
